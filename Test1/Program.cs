57be8f43b94214faa21cc2e0ddd39c3c535007eb
userz2 <126as500@gmail.com>
2019-05-22 19:57:20 +0300

    Правки Консоли

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -14,38 +14,44 @@ namespace Test1
             string a = "";
             while (a!="6")
             {
-                Console.Clear();
+                
                 Console.WriteLine("Выберите действие:\n 1.Создать книгу;\n 2.Список авторов;\n 3.Список книг; \n 4.Поиск авторов по книге; \n 5.Удаление книги по номеру;\n 6.Exit");
 
                 switch (Console.ReadLine())
                 {
                     case "1":
                         {
+                            Console.Clear();
                             books.Create();
                         }
                         break;
                     case "2":
                         {
+                            Console.Clear();
                             books.Autor();
                         }
                         break;
                     case "3":
                         {
+                            Console.Clear();
                             books.Book();
                         }
                         break;
                     case "4":
                         {
+                            Console.Clear();
                             books.Select();
                         }
                         break;
                     case "5":
                         {
+                            Console.Clear();
                             books.Delete();
                         }
                         break;
                     case "6":
                         {
+                            Console.Clear();
                             a = "6";
                         }
                         break;
